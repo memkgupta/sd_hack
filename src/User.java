0c94b35c855556da986d257c34fbc01a2de4b7f1
faraz2134 <cse23201@glbitm.ac.in>
2026-02-25 21:11:37 +0530

    userservice

--- src/User.java
+++ src/User.java
@@ -1,12 +1,18 @@
 
-public class User {
-    private String id;
 
-    public String getId() {
-        return id;
+class User {
+    private String id;
+    private String name;
+
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getId() { return id; }
+    public String getName() { return name; }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/UserService.java
+++ src/UserService.java
@@ -1,2 +1,38 @@
-public class UserService {
+import java.util.*;
+
+class UserService {
+    private Map<String, User> users = new HashMap<>();
+
+    public void createUser(String id, String name) {
+        if (users.containsKey(id)) {
+            System.out.println("User already exists!");
+            return;
+        }
+        users.put(id, new User(id, name));
+    }
+
+    
+    public User getUser(String id) {
+        return users.get(id);
+    }
+
+
+    public void updateUser(String id, String name) {
+        User user = users.get(id);
+        if (user != null) {
+            user.setName(name);
+        }
+    }
+
+ 
+    public void deleteUser(String id) {
+        users.remove(id);
+    }
+
+  
+    public void showUsers() {
+        for (User u : users.values()) {
+            System.out.println(u.getId() + " " + u.getName());
+        }
+    }
 }
